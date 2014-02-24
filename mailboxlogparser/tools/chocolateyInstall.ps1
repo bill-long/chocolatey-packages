c220501aacc4cb95b9892c34ba87ca7068ef3f03
Bill Long <bill@blackout.us>
2014-02-24 15:11:48 -0600

    MailboxLogParser: Added package

---
+++ mailboxlogparser/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿try {
+  $packageName = 'mailboxlogparser'
+  $binRoot = Get-BinRoot
+  Write-Debug "Bin Root is $binRoot"
+  $installDir = Join-Path "$binRoot" "$packageName"
+  $url = 'https://mailboxlogparser.codeplex.com/downloads/get/742261'
+  
+  Install-ChocolateyZipPackage $packageName $url $installDir
+
+  Write-ChocolateySuccess 'mailboxlogparser'
+} catch {
+  Write-ChocolateyFailure 'mailboxlogparser' $($_.Exception.Message)
+  throw
+}
