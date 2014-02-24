eff3851838eeb4cb54ef91b4f6a35f9a1d1d9da9
Bill Long <bill@blackout.us>
2014-02-24 13:50:21 -0600

    Log Parser Studio: Created package

---
+++ logparserstudio/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿try {
+  $packageName = 'logparserstudio'
+  $binRoot = Get-BinRoot
+  Write-Debug "Bin Root is $binRoot"
+  $installDir = Join-Path "$binRoot" "$packageName"
+  $url = 'http://gallery.technet.microsoft.com/Log-Parser-Studio-cd458765/file/53920/5/LPSV2.D2.zip'
+
+  Install-ChocolateyZipPackage $packageName $url $installDir
+
+  Write-ChocolateySuccess 'logparserstudio'
+} catch {
+  Write-ChocolateyFailure 'logparserstudio' $($_.Exception.Message)
+  throw
+}
