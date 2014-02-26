f5e12c50baa9f9d6ba8489f8363ee071b102f35d
Bill Long <bill@blackout.us>
2014-02-26 16:35:10 -0600

    WindowsPhoneSDK: Added package

---
+++ WindowsPhoneSDK/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'WindowsPhoneSDK'
+$installerType = 'EXE'
+$url = 'http://download.microsoft.com/download/9/3/8/938A5074-461F-4E3D-89F4-5CE2F42C1E36/WPexpress_full.exe'
+$silentArgs = '/Passive' # We're using /passive instead of /quiet so we can see progress, because this installer can take a really long time
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
