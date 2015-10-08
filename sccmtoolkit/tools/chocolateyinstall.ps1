188f8735df2ace5b5a8e2dd6df4e39e3365258f4
Bill Long <bilong@microsoft.com>
2015-10-07 20:57:29 -0500

    Add package for SCCM Toolkit

---
+++ sccmtoolkit/tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'sccmtoolkit'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://download.microsoft.com/download/D/3/8/D3842EDD-D2AF-4035-8C0F-C07984F4134F/ConfigMgrTools.msi'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+
+  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
