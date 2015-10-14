c16bbf3f5d869387d35e8953783c4dc841ced526
Bill Long <bilong@microsoft.com>
2015-10-14 15:48:10 -0500

    Add package for apexsql search

---
+++ apexsqlsearch/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
+#   $f='c:\path\to\thisFile.ps1'
+#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'apexsqlsearch'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.apexsql.com/zips/ApexSQLSearch.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+  silentArgs    = "/SILENT"
+  validExitCodes= @(0, 3010, 1641)
+
+  softwareName  = 'ApexSQL Search 2014'
+}
+
+Install-ChocolateyPackage @packageArgs
