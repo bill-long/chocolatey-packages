58c41618a62ef499d0bbc193c20f40eeacda04cc
Bill Long <bilong@microsoft.com>
2016-01-22 10:32:01 -0600

    Update mfcmapi to latest

--- mfcmapi/tools/chocolateyInstall.ps1
+++ mfcmapi/tools/chocolateyInstall.ps1
@@ -2,14 +2,15 @@
   $packageName = 'mfcmapi'
   $binRoot = Get-BinRoot
   Write-Debug "Bin Root is $binRoot"
-  $installDir = Join-Path "$binRoot" "$packageName"
+  $installDir = Join-Path "$binRoot" "$packageName"
+
   # We're going to copy both the 32-bit and 64-bit executables, because
   # the bitness of Outlook won't necessarily match the bitness of the
   # OS.
   
-  # These links map to February 2014 Release, build 15.0.0.1041
-  $x86Url = 'http://mfcmapi.codeplex.com/downloads/get/795660'
-  $x64Url = 'http://mfcmapi.codeplex.com/downloads/get/795662'
+  # These links map to September 2015 Release, build 15.0.0.1043
+  $x86Url = 'http://mfcmapi.codeplex.com/downloads/get/1482127'
+  $x64Url = 'http://mfcmapi.codeplex.com/downloads/get/1482129'
 
   $x86InstallDir = Join-Path "$installDir" "x86"
   $x64InstallDir = Join-Path "$installDir" "x64"
